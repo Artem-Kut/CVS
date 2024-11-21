b1f3d24109d53a92de4af8b3752b0e5c74b9b657
Artem-Kut <artem.kutasevych@gmail.com>
2024-11-21 11:21:02 -0500

    [-] Start project

---
+++ CVSTest/CVSTest/CVSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSTestApp.swift
+//  CVSTest
+//
+//  Created by Artem Kutasevych on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
